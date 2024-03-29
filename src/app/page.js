3bf730906a35e75cac4d850c69955d0baae68a94
claudiopome <claudiopome@gmail.com>
2024-03-29 19:53:48 +0100

    add info to docs, minor styling.

--- src/app/page.js
+++ src/app/page.js
@@ -4,58 +4,46 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1 className={styles.headline}>Trips API</h1>
-      <p className={styles.subhead}>The documentation</p>
+      <p className={styles.subhead}>Documentation</p>
 
       <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {endpoints.map((endpoint, index) => (
+          <a
+            href="#"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              <code>
+                {endpoint.name} <span>-&gt;</span>
+              </code>
+            </h2>
+            <p>{endpoint.description}</p>
+            {endpoint.parameters && (
+              <>
+                <br />
+                <p>Parameters:</p>
+                <ul>
+                  {endpoint.parameters.map((parameter, parameterIndex) => (
+                    <li key={parameterIndex}>
+                      <b>{parameter.name}</b>: {parameter.description}
+                    </li>
+                  ))}
+                </ul>
+              </>
+            )}
+            {endpoint.response && (
+              <>
+                <br />
+                <p>Example response:</p>
+                <pre>
+                  <code>{endpoint.response}</code>
+                </pre>
+              </>
+            )}
+          </a>
+        ))}
       </div>
     </main>
   );
